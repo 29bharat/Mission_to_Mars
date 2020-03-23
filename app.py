04320341e72b702aa2d2236529fbdce2c78878ab
Agarwal <Heena.Agarwal2@transformco.com>
2020-03-22 23:01:50 -0700

    Mars Html

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
-# Setting up home page
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
 
-#Setting up the Scrape Route
+
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
+   print(mars_data)
    mars.update({}, mars_data, upsert=True)
    return "Scraping Successful!"
 
